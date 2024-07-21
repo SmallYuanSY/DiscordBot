682db7a23e55606209d0b35f9ba46bac82c1dc3d
まぅSmallYuan <alan6716s@gmail.com>
2024-07-21 12:27:39 +0800

    指令運作邏輯與bot架構設定

---
+++ src/commands/ping/index.js
@@ -0,0 +1,8 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+export const command = new SlashCommandBuilder().setName('ping').setDescription('Ping');
+
+export const action = async (ctx) => 
+    {
+        ctx.reply('Pong!');
+    };
---
+++ src/store/app.js
@@ -0,0 +1,7 @@
+import {defineStore} from 'pinia'
+
+export const useAppStore = defineStore('counter', {
+  state: () => ({}),
+  getters: {},
+  actions: {},
+})
